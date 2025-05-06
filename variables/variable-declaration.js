b0f6655db18ae96b9c3d3451723e365fa49f1bce
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-06 17:11:10 +0530

    js variable declaration

---
+++ variables/variable-declaration.js
@@ -0,0 +1,11 @@
+var age = 25;
+let name = "Amal";
+const address = "Colombo";
+
+age = 30; 
+name = "John"; 
+// address = "Kandy"; // can not reassign a variable declared with const 
+
+console.log(age);
+console.log(name);
+console.log(address);
